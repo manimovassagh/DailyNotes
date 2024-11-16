54c46954e5316617b37c5dea58b262ee5de55c61
Mani Movassagh <mani.mgh@gmail.com>
2024-11-17 00:45:15 +0100

    runner

--- Controllers/Customer.cs
+++ Controllers/Customer.cs
@@ -1,14 +1,13 @@
-using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DailyNotes.Models;
 
 namespace DailyNotes.Controllers;
 
-public class CustomerController(ILogger<HomeController> logger): Controller
+public class CustomerController(ILogger<CustomerController> logger) : Controller
 {
-    
-    private readonly ILogger<HomeController> _logger;
+    private readonly ILogger<CustomerController> _logger = logger;
+
 
 
     public IActionResult Custom()
@@ -16,5 +15,9 @@ public class CustomerController(ILogger<HomeController> logger): Controller
         return View();
     }
 
-    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
