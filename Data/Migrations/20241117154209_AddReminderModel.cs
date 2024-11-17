d83b5a5fb4943c8c381d7cb056299fc62787fc42
Mani Movassagh <mani.mgh@gmail.com>
2024-11-17 17:05:38 +0100

    add

---
+++ Data/Migrations/20241117154209_AddReminderModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DailyNotes.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReminderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Reminders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    Content = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reminders", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Reminders");
+        }
+    }
+}
---
+++ Data/Migrations/20241117154529_SeedReminderData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace DailyNotes.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedReminderData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Reminders",
+                columns: new[] { "Id", "Content", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Don't forget the team meeting at 10 AM.", "Meeting Reminder" },
+                    { 2, "Visit Dr. Smith at 3 PM.", "Doctor Appointment" },
+                    { 3, "Buy milk, eggs, and bread.", "Grocery Shopping" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Reminders",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Reminders",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Reminders",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
